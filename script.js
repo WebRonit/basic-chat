3c3d0fb578ccf400b8c724760acbc0676f4f1ed2
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-03-07 19:10:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("https://simple-chat-room-75ta.onrender.com") 
+const socket = io("https://simple-chat-room-75ta.onrender.com") // https://simple-chat-room-75ta.onrender.com
 
 const form  = document.getElementById('msg-form');
 const msgInp  = document.getElementById('inp');
@@ -57,23 +57,49 @@ const append = (userName, msg, pos) => {
 }
 
 imgInp.onchange = function sendImage() {
+
     const fileInput = imgInp;
     const file = fileInput.files[0];
 
     if (file) {
-        const reader = new FileReader();
-
-        reader.onload = function(event) {
-            imgData = event.target.result;
-            hasImg = true;
-            previewImg.src = imgData
-            previewImgCont.style.display = 'block'
-           // showImg.src = imgData
-            
+        console.log('uploding..')
+        if (file.size > 500*1024) {
+            new Compressor(file, {
+                quality: 0.7, 
+                maxWidth: 800,
+                maxHeight: 600,
+                mimeType: 'image/jpeg', 
+                success: function(result) {
+                    const reader = new FileReader();
+                    reader.onload = function(event) {
+                        imgData = event.target.result; 
+                        previewImg.src = imgData
+                        previewImgCont.style.display = 'block'
+                        console.log('Compressed');
+                    };
+                    reader.readAsDataURL(result);
+                },
+                error: (error) => {
+                    console.error('Image compression failed:', error);
+                },
+            });
         }
 
-        reader.readAsDataURL(file);
+        else{
+           const reader = new FileReader();
+           reader.onload = function(event) {
+               imgData = event.target.result;
+               previewImg.src = imgData
+               previewImgCont.style.display = 'block'
+               
+               console.log('original')
+           }
+           reader.readAsDataURL(file);
+        }
+        hasImg = true;
+       
     }
+
 }
 
 function closePreview (){
@@ -92,6 +118,7 @@ function appendImg(userName, txt, imgURL, pos){
 
     if(pos == 'left'){
        imgCont.style.float = 'left'
+       imgCont.style.backgroundColor = 'rgb(240,240,240)';
     }
 
     else{
